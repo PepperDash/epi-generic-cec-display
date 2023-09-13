e99bbbbd8f9ba23b2fd3622821c583f0cc19a651
Jason DeVito <jdevito@pepperdash.com>
2023-09-13 14:01:04 -0500

    chore: repo cleanup
    Removed Simpl Example with Modbus TCP Example.
    
    Renamed plugin folder `src`.

---
+++ src/CecDisplayDriverControllerFactory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using PepperDash.Core;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Config;
+
+namespace PepperDash.Plugin.Display.CecDisplayDriver
+{
+    public class CecDisplayDriverControllerFactory : EssentialsPluginDeviceFactory<CecDisplayDriverDisplayController>
+    {
+        public CecDisplayDriverControllerFactory()
+        {
+			MinimumEssentialsFrameworkVersion = "1.6.7";
+            TypeNames = new List<string> {"GenericCecDisplay"};
+        }
+
+        #region Overrides of EssentialsDeviceFactory<SamsungMdcDisplayController>
+
+        public override EssentialsDevice BuildDevice(DeviceConfig dc)
+        {
+
+            var comms = CommFactory.CreateCommForDevice(dc);
+
+            if (comms == null)
+            {
+                Debug.Console(0, Debug.ErrorLogLevel.Error, "Unable to create comms for device {0}", dc.Key);
+                return null;
+            }
+
+            var config = dc.Properties.ToObject<CecDisplayDriverPropertiesConfig>();
+
+            if (config != null)
+            {
+                return new CecDisplayDriverDisplayController(dc.Key, dc.Name, config, comms);
+            }
+
+            Debug.Console(0, Debug.ErrorLogLevel.Error, "Unable to deserialize config for device {0}", dc.Key);
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ src/CecDisplayDriverControllerJoinMap.cs
@@ -0,0 +1,19 @@
+﻿using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Bridges;
+
+namespace PepperDash.Plugin.Display.CecDisplayDriver
+{
+	public class CecDisplayDriverControllerJoinMap : DisplayControllerJoinMap
+	{
+
+
+
+		/// <summary>
+		/// Display controller join map
+		/// Some specific adds for Samsung Temperature and Brightness control and feedback
+		/// </summary>
+		public CecDisplayDriverControllerJoinMap(uint joinStart) : base(joinStart, typeof(CecDisplayDriverControllerJoinMap))
+		{
+        }
+	}
+}
